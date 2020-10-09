5ba356b10ab93100fed094d89a086bb4b11fc63a
mdmahmudurrahman <mailtommrr@gmail.com>
2020-10-09 10:43:38 +0200

    Initial Commit

---
+++ src/main/java/com/amigoscode/ssecurity/ssapp/SsappApplication.java
@@ -0,0 +1,13 @@
+package com.amigoscode.ssecurity.ssapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsappApplication.class, args);
+	}
+
+}
