2a3d83ea9edb420bfd0665c589679b427971d32c
mdmahmudurrahman <mailtommrr@gmail.com>
2020-10-11 20:50:31 +0200

    In-memory user and encrypt password

--- src/main/java/com/amigoscode/ssecurity/ssapp/security/ApplicationSecurityConfig.java
+++ src/main/java/com/amigoscode/ssecurity/ssapp/security/ApplicationSecurityConfig.java
@@ -1,14 +1,27 @@
 package com.amigoscode.ssecurity.ssapp.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
-
+	private final PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder){
+		this.passwordEncoder = passwordEncoder;
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -18,5 +31,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 			.authenticated()
 			.and()
 			.httpBasic();
+	}
+
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails user1 = User.builder()
+								.username("user1")
+								.password(passwordEncoder.encode("password1"))
+								.roles("STUDENT") // ROLE_STUDENT
+								.build();
+		return new InMemoryUserDetailsManager(user1);
 	}
 }
