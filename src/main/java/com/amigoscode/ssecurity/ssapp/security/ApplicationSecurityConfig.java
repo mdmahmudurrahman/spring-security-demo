8b99bb326b827cbcceb11af141e965aa8ed6c349
mdmahmudurrahman <mailtommrr@gmail.com>
2020-10-11 17:31:01 +0200

    White list url using antMatchers

--- src/main/java/com/amigoscode/ssecurity/ssapp/security/ApplicationSecurityConfig.java
+++ src/main/java/com/amigoscode/ssecurity/ssapp/security/ApplicationSecurityConfig.java
@@ -12,10 +12,11 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+			.antMatchers("/", "index", "/css/*", "/js/*")
+			.permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
 			.httpBasic();
 	}
-	
 }
